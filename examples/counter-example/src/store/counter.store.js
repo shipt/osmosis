bd77b1f03979d36fd237e52511c3171d439ab0c0
Jared Benskins <jaredbenskins@C02CC889MD6N.fios-router.home>
2020-07-22 16:00:16 -0400

    add counter files

---
+++ examples/counter-example/src/store/counter.store.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { setupStore } from 'osmosis';
+
+const useCounterContainer = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return {
+    state: {
+      count
+    },
+    increment,
+    decrement
+  };
+};
+
+let [CounterContext, wrapCounter, counterRef] = setupStore(useCounterContainer);
+
+export { CounterContext, wrapCounter, counterRef };
