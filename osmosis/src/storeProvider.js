feebe16d6a60424da00b910c1d697005c9a3fab5
Jakhongir Khusanov <khusanov.jakhongir@gmail.com>
2021-03-27 12:52:04 -0700

    Check StoreProvider arguments before using them

--- osmosis/src/storeProvider.js
+++ osmosis/src/storeProvider.js
@@ -1,4 +1,8 @@
 export const StoreProvider = (storeProviders, wrappedComponent) => {
+  if (!storeProviders || !wrappedComponent) {
+    throw new Error('could not find array of store providers or your wrapped root component; please ensure your provide at least an empty array and your root component to the StoreProvider from osmosis');
+  }
+
   storeProviders.reverse().forEach(provider => (wrappedComponent = provider(wrappedComponent)));
   return wrappedComponent;
 };
