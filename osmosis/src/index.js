baddf1c3c7ae9f3a5fa5f2618c3fd42b80723f6b
chaceburnette <chace@shipt.com>
2021-04-16 12:05:15 -0500

    remove deprecated class container logic (#56)

--- osmosis/src/index.js
+++ osmosis/src/index.js
@@ -1,10 +1,9 @@
 import { StoreProvider } from './storeProvider.js';
-import { Container, setupStore } from './setupStore.js';
+import { setupStore } from './setupStore.js';
 import { usePersistedState, configureUsePersistedState } from './usePersistedState.js';
 
-export { Container, setupStore, StoreProvider, usePersistedState, configureUsePersistedState };
+export { setupStore, StoreProvider, usePersistedState, configureUsePersistedState };
 export default {
-  Container,
   setupStore,
   StoreProvider,
   usePersistedState,
