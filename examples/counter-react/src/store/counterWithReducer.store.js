4d76a2749351ffde156fa786c95af51ac8c530f2
Chace <chace@shipt.com>
2021-06-10 21:08:39 -0500

    more examples and cleanup

---
+++ examples/counter-react/src/store/counterWithReducer.store.js
@@ -0,0 +1,28 @@
+import { useReducer } from 'react';
+import { setupStore } from '@shipt/osmosis';
+
+const initialState = {count: 0};
+
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      throw new Error();
+  }
+}
+
+const useCounterWithReducerStore = () => {
+  const [counterState, dispatch] = useReducer(counterReducer, initialState);;
+
+  return {
+    counterState,
+    dispatch
+  };
+};
+
+let [CounterWithReducerContext, CounterWithReducerWrapper, counterWithReducerRef] = setupStore(useCounterWithReducerStore);
+
+export { CounterWithReducerContext, CounterWithReducerWrapper, counterWithReducerRef};
