65eda4da51faf18476e427fdfd289d39692f14c4
Kyle Easterling <keasterling@shipt.com>
2021-12-06 13:39:25 -0600

    add example for using the storeRef with nested stores

--- examples/counter-react/src/counterByName.js
+++ examples/counter-react/src/counterByName.js
@@ -5,6 +5,9 @@ const Counter = ({ name }) => {
   const [counterContext] = useContext(CounterStore.Context);
   let { count } = counterContext.state;
 
+  // this isn't necessary, but this demonstrates how a store ref would be used if a storeKey is provided
+  const incrementCount = () => CounterStore[name].incrementCount();
+
   return (
     <div data-testid="counter-wrap">
       <p>
@@ -13,7 +16,7 @@ const Counter = ({ name }) => {
       <button data-testid="decrement" onClick={counterContext.decrementCount}>
         -
       </button>
-      <button data-testid="increment" onClick={counterContext.incrementCount}>
+      <button data-testid="increment" onClick={incrementCount}>
         +
       </button>
     </div>
