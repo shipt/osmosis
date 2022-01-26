95008be7031699b34caa4192bd659df221b3547e
Chace <chace@shipt.com>
2022-01-25 21:27:27 -0600

    update counter examples to work with non-array syntax

--- examples/counter-react/src/persistedCounter.js
+++ examples/counter-react/src/persistedCounter.js
@@ -2,16 +2,16 @@ import React, { useContext } from 'react';
 import { CounterStore } from './store';
 
 const PersistedCounter = () => {
-  const [counterContext] = useContext(CounterStore.Context);
-  let { persistedCount } = counterContext.state;
+  const counterStore = useContext(CounterStore.Context);
+  let { persistedCount } = counterStore.state;
 
   return (
     <div data-testid="counter-wrap">
       <p>Persisted Count: {persistedCount}</p>
-      <button data-testid="decrement" onClick={counterContext.decrementPersistedCount}>
+      <button data-testid="decrement" onClick={counterStore.decrementPersistedCount}>
         -
       </button>
-      <button data-testid="increment" onClick={counterContext.incrementPersistedCount}>
+      <button data-testid="increment" onClick={counterStore.incrementPersistedCount}>
         +
       </button>
     </div>
