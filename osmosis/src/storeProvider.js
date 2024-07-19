b8f29a8171d7e70e464980ae023c8df30536162d
Chace Burnette <chace@shipt.com>
2024-07-19 11:04:39 -0500

    support just passing store to store provider

--- osmosis/src/storeProvider.js
+++ osmosis/src/storeProvider.js
@@ -1,16 +1,19 @@
 /**
  * @param {Object[]} storeProviders
- * @param {Object} componentToWrap 
+ * @param {Object} componentToWrap
  * @returns {Object} wrappedComponent
  */
 export const StoreProvider = (storeProviders, wrappedComponent) => {
-  if(!Array.isArray(storeProviders)) throw new Error('StoreProvider requires an array of wrapper functions')
-  if(!wrappedComponent) throw new Error('StoreProvider requires a component to wrap')
+  if (!Array.isArray(storeProviders)) throw new Error('StoreProvider requires an array of wrapper functions');
+  if (!wrappedComponent) throw new Error('StoreProvider requires a component to wrap');
 
   try {
-    storeProviders.reverse().forEach(provider => (wrappedComponent = provider(wrappedComponent)));
+    storeProviders.reverse().forEach(provider => {
+      const providerFunction = provider.Provider || provider;
+      wrappedComponent = providerFunction(wrappedComponent);
+    });
     return wrappedComponent;
-  } catch(error) {
-    throw new Error(`StoreProvider encountered an error: ${error.message}`)
+  } catch (error) {
+    throw new Error(`StoreProvider encountered an error: ${error.message}`);
   }
 };
